0aac90f2435893ec40e3a8434a53aad6aa544cd8
KaterynaBachkalo <kozlovets88@gmail.com>
2024-06-04 23:39:59 +0200

    fix utils to ts

---
+++ src/utils/validators.ts
@@ -0,0 +1,60 @@
+import Joi from "joi";
+
+const addContactSchema = Joi.object({
+  name: Joi.string()
+    .min(3)
+    .max(20)
+    .required()
+    .messages({ "any.required": "missing required name field" }),
+
+  email: Joi.string()
+    .email()
+    .required()
+    .messages({ "any.required": "missing required email field" }),
+
+  phone: Joi.string()
+    .min(3)
+    .max(15)
+    .required()
+    .messages({ "any.required": "missing required phone field" }),
+});
+
+const updateStatusSchema = Joi.object({
+  favorite: Joi.boolean()
+    .required()
+    .messages({ "any.required": "missing required favorite field" }),
+});
+
+const addUserSchema = Joi.object({
+  password: Joi.string().min(6).required().messages({
+    "any.required": "Set password for user",
+  }),
+
+  email: Joi.string()
+    .email()
+    .required()
+    .messages({ "any.required": "Email is required" }),
+});
+
+const updateSubSchema = Joi.object({
+  subscription: Joi.string()
+    .required()
+    .valid("starter", "pro", "business")
+    .messages({ msg: "This subscription doesn't exist" }),
+});
+
+const contactListSchema = Joi.object({
+  limit: Joi.number(),
+
+  page: Joi.number(),
+
+  favorite: Joi.boolean(),
+});
+
+export default {
+  contactListSchema,
+  updateSubSchema,
+  addUserSchema,
+  updateStatusSchema,
+  addContactSchema,
+};
