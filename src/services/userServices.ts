a746d890d9492cc7645f8c908d3804fc1c392928
KaterynaBachkalo <kozlovets88@gmail.com>
2024-07-05 17:12:00 +0300

    fix

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -52,7 +52,7 @@ const login = async ({ email, password }: LoginParams) => {
   });
 
   const refreshToken = jwt.sign({ id: user.id }, serverConfig.jwtSecret, {
-    expiresIn: process.env.NODE_ENV === "production" ? "7d" : "2m",
+    expiresIn: process.env.NODE_ENV === "production" ? "7d" : "1d",
   });
 
   await User.findByIdAndUpdate(user.id, { accessToken, refreshToken });
